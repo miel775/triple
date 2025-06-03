a13287e59fbba6f8d9c65a028ca03ccfd70e1725
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-06-03 12:17:30 +0200

    Node JS opgezet
    De basis is gelegd
    
    #10

--- server.js
+++ server.js
@@ -1,1 +1,28 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+
+import { Liquid } from 'liquidjs';
+
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+const engine = new Liquid()
+app.engine('liquid', engine.express())
+
+app.set('views', './views')
+
+app.get('/', async function (request, response) {
+    response.render('index.liquid')
+});
+
+app.set('port', process.env.PORT || 8000)
+
+// Start Express op, gebruik daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function () {
+
+  console.log('Dit is de PokeApp, gonna catch them all!')
+  console.log(`http://localhost:${app.get('port')}`)
+})
+
