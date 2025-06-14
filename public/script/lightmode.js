050138a9bacb22a6219ed74d0794a1bc8b19894a
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-06-14 20:14:33 +0200

    Color scheme
    Bij de stylsheet heb ik kleuren toegevoegd.
    
    #13

---
+++ public/script/lightmode.js
@@ -0,0 +1,6 @@
+const sunButton = document.getElementById('sun-button');
+const moonButton = document.getElementById('moon-button');
+
+sunButton.addEventListener('click', ( {
+    document.toggl
+}))
