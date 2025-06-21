ea8613782fd66c96de33bff48a9d4c0461c312a9
Miel <miel.van.sleeuwen.van.sleeuwen@hva.nl>
2025-06-21 08:09:46 +0200

    Search
    uitleg
    
    #20

--- public/script/prevents-default.js
+++ public/script/prevents-default.js
@@ -1,25 +1,38 @@
 document.getElementById('form-search').addEventListener('submit', async function(e) {
     e.preventDefault();
+    // wanneer je iets zoekt (dus op enter klikt refresh de pagina niet)
 
     const input = document.getElementById('search-input');
+    // selecteer de search input
     const query = input.value.trim().toLowerCase();
 
     if (!query) return;
 
+    // laat de content zien
     try {
+
+        // de request van de server wordt opgevraagd ner war je hebt ingevuld in de search input
+        // encodeURIcomponent zorgt ervoor dat spaties en speciale tekens in de search input kunnen
         const response = await fetch(`/search?p=${encodeURIComponent(query)}`);
         const html = await response.text();
 
         const parser = new DOMParser();
+        // de html slaat de search input op in de server
         const doc = parser.parseFromString(html, 'text/html');
+        // het wordt omgezet tot een virtueel html document
         const newContent = doc.getElementById('overview');
+        // de resultaten worden weergegeven op de pagina
 
         if (newContent) {
             document.getElementById('overview').innerHTML = newContent.innerHTML;
         } else {
-            document.getElementById('overview').innerHTML = '<p>No results found.</p>';
+            // als er geen resultaten worden weergegeven laat dit dan zien
+            document.getElementById('overview').innerHTML = 
+            '<p> No results found </p>';
         }  
     } catch (err) {
-        document.getElementById('overview').innerHTML = "views/empty.liquid";
+
+        // wanneer heel de functie niet werkt
+        document.getElementById('overview').innerHTML = '<p> The search function is not working </p>';
     }
 });
